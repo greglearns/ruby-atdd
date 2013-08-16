9c71d506cb6a926b47753615c8160e63fd0853d2
Greg Edwards <greg@greglearns.com>
2013-08-15 18:37:01 -0600

    Refactor partial for base.rb adding Scenario(s)Parser classes.

--- lib/base.rb
+++ lib/base.rb
@@ -1,52 +1,49 @@
 require_relative 'source_code_matcher_extractor'
+require 'ostruct'
+require 'dotenv'
 
 module RubyAtdd
 
   class Base
 
     def self.scenarios(attrs)
-      file_name = attrs.fetch(:file_name, nil)
-      called_by = caller.first.split(':')[0]
-      require 'dotenv'
-      Dotenv.load ".env.#{ENV['ENV']}", '.env'
+      load_dot_env
+      called_by = called_by(caller)
       subject = Atdd::CreateServer.new({uri_base: ENV['BASE_URL']})
 
-      puts '', '-'*30, "TEST RUN STARTED", File.basename(called_by), "#{called_by}", "URL_BASE=#{ENV['BASE_URL']}", Time.now.to_s, '-'*30, ''
+      puts '', '-'*30, "TEST RUN STARTED", File.basename(called_by.file), "#{called_by.file}", "URL_BASE=#{ENV['BASE_URL']}", Time.now.to_s, '-'*30, ''
 
+      results = ScenariosParser.new(get_scenarios_str(attrs)).map do |scenario_str|
+        subject.scenario(scenario_str)
+        .tap{|r| puts } # blank line between scenario results
+      end
+
+      failing_tests_count = results.reduce(:+)
+      .tap{|cnt| puts "#{cnt} tests failed." }
+      exit(failing_tests_count)
+    end
+
+    def self.load_dot_env
+      Dotenv.load ".env.#{ENV['ENV']}", '.env'
+    end
+
+    def self.get_scenarios_str(attrs)
+      file_name = attrs.fetch(:file_name, nil)
       if file_name
         str = File.read(file_name)
         str = str.split("__END__")[1]
       else
         str = attrs.fetch(:str)
       end
-
-      scenarios = str.split("\n\n").compact.map{|e| e.strip.empty? ? nil : e }.compact
-
-      results = scenarios.map do |scenario_str|
-        subject.scenario(scenario_str)
-        .tap{|r| puts } # blank line between scenario results
-      end
-
-      failing_tests_count = results.reduce(:+)
-      puts "#{failing_tests_count} tests failed."
-      exit(failing_tests_count)
+      str
     end
 
     def scenario(str)
       response_code = 0
       begin
-        ii = str.each_line
-        loop do
-          line = CleanLine.new(ii.next).strip
-          next if line.skip?
-          print line
-          line.skip_first_word!
-          multiline_text = extract_multiline_text(ii)
+        ScenarioParser.new(str).each_line do |line, multiline_text|
           execute_line(line.to_s, multiline_text)
         end
-      # rescue MiniTest::Assertion => e
-      #   puts "FAIL: #{e.message} #{'-'*30}"
-      #   response_code = 1
       rescue Exception => e # must rescue Exception because Minitest::Assertion subclasses Exception
         puts "FAIL: #{e.message}"
         puts e.backtrace unless e.class.to_s == 'Minitest::Assertion'
@@ -56,29 +53,17 @@ module RubyAtdd
       response_code
     end
 
-    def extract_multiline_text(ii)
-      return unless CleanLine.new(ii.peek).is_fence?
-      indent = CleanLine.new(ii.next).compute_indent_size
-      lines = []
-      loop do
-        line = CleanLine.new(ii.next)
-        break if line.is_fence?
-        line.remove_indent!(indent)
-        lines << line.to_s
-      end
-      str = lines.join
-      str.empty? ? nil : str
-    end
-
     def self.method_added(name)
-      hst = caller
-      file_name = hst.first.split(':')[0]
-      file_line = hst.first.split(':')[1].to_i
-      extractor = SourceCodeMatcherExtractor.new(file_name: file_name, line: file_line)
+      extractor = SourceCodeMatcherExtractor.new(called_by(caller).to_h)
       matcher_lines = extractor.extract
       @matchers ||= []
       @matchers << [name, matcher_lines] unless matcher_lines.empty?
       super
+    end
+
+    def self.called_by(history)
+      last = history.first.split(':')
+      OpenStruct.new({file: last[0], line: last[1].to_i})
     end
 
     def self.matchers
@@ -109,6 +94,58 @@ module RubyAtdd
         (match = [list[0], actual_match]; break) if actual_match
       end
       match
+    end
+
+  end
+
+  class ScenariosParser
+    def initialize(str)
+      @scenarios = breakup_scenarios(str)
+    end
+
+    def map
+      @scenarios.map{|scenario_str| yield scenario_str }
+    end
+
+    private
+
+    def breakup_scenarios(str)
+      str.split("\n\n").compact.map{|e| e.strip.empty? ? nil : e }.compact
+    end
+
+  end
+
+  class ScenarioParser
+    def initialize(str)
+      @str = str
+    end
+
+    def each_line(&block)
+      ii = @str.each_line
+      loop do
+        line = CleanLine.new(ii.next).strip
+        next if line.skip?
+        print line
+        line.skip_first_word!
+        multiline_text = extract_multiline_text(ii)
+        yield line.to_s, multiline_text
+      end
+    end
+
+    private
+
+    def extract_multiline_text(ii)
+      return unless CleanLine.new(ii.peek).is_fence?
+      indent = CleanLine.new(ii.next).compute_indent_size
+      lines = []
+      loop do
+        line = CleanLine.new(ii.next)
+        break if line.is_fence?
+        line.remove_indent!(indent)
+        lines << line.to_s
+      end
+      str = lines.join
+      str.empty? ? nil : str
     end
 
   end
